95fe51ecdbe06773539d089e0548f86e80437412
Mohamet Amine Ndiaye <leatdev@github.com>
2024-01-30 12:08:09 -0500

    Initial Commit

---
+++ imc/imcApp.swift
@@ -0,0 +1,17 @@
+//
+//  imcApp.swift
+//  imc
+//
+//  Created by Mohamet amine Ndiaye on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct imcApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
